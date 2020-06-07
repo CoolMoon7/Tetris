ab0e6141be27d91ec7b8c2c14c52b8f8c6981d63
Cole <colebonawitz@gmail.com>
2020-06-07 14:07:01 -0400

    added pheonix server config

--- tetrisui/lib/tetrisui_web/live/tetris_live.ex
+++ tetrisui/lib/tetrisui_web/live/tetris_live.ex
@@ -1,18 +1,20 @@
-#defmodule TetrisuiWeb.TetrisLive do
-#  use Pheonix.LiveView
-#  import Pheonix.HTML, only: [raw: 1]
-#
-#  def mount(_session, socket) do
-#      :ok,
-#    assign(socket,
-#      hello: world,
-#      next: :key
-#      )
-#    }
-#  end
-#
-##    ~L"""
-#      <h1>Hello, world</h1>
-#    """
-#  end
-#end
+defmodule TetrisuiWeb.TetrisLive do
+  use Pheonix.LiveView
+  import Pheonix.HTML, only: [raw: 1]
+
+  def mount(_session, socket) do
+    {
+        :ok,
+      assign(socket,
+        hello: world,
+        next: :key
+        )
+    }
+  end
+
+  def render(assigns) do
+    ~L"""
+      <h1>Hello, world</h1>
+    """
+  end
+end
